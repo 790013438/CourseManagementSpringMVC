c97295f2019b03932bb5d3390c137539e97e36dc
790013438 <790013438@qq.com>
2017-09-13 20:46:17 +0800

    add controller methods
    When an addCourse request is made, we want to serve the
    above mentioned page that displays the input form.

--- src/main/java/snippets/jee/course_management/controller/CourseController.java
+++ src/main/java/snippets/jee/course_management/controller/CourseController.java
@@ -3,9 +3,11 @@ package snippets.jee.course_management.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import snippets.jee.course_management.dao.CourseDAO;
+import snippets.jee.course_management.dto.CourseDTO;
 
 @Controller
 public class CourseController {
@@ -17,5 +19,14 @@ public class CourseController {
     public String getCourses (Model model) {
         model.addAttribute("courses", courseDAO.getCourses());
         return "courses";
+    }
+
+    @RequestMapping("/addCourse")
+    public String addCourse (@ModelAttribute("course") CourseDTO course, Model model) {
+        if (course == null) {
+            course = new CourseDTO();
+        }
+        model.addAttribute("course", course);
+        return "addCourse";
     }
 }
