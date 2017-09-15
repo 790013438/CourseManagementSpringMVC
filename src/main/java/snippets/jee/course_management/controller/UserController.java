dfdff1b4463723521bb32a2f2f5711438d19ad84
790013438 <790013438@qq.com>
2017-09-15 20:11:21 +0800

    @RequestMapping (value="/login", method=RequestMethod.POST)
    Note that we have specified the method attribute in the @RequestMapping
    annotation. When the request URL is /login and the HTTP request type is
    GET, only then will the login method be called. This method would not be
    called if the POST request is sent from the client. In the login method,
    we create an instance of UserDTO and add it to Model so that it is
    accessible to view.  We will add a method to handle the POST request
    from the login page. We will keep the same URL, that is, /login

--- src/main/java/snippets/jee/course_management/controller/UserController.java
+++ src/main/java/snippets/jee/course_management/controller/UserController.java
@@ -2,6 +2,7 @@ package snippets.jee.course_management.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,4 +19,18 @@ public class UserController {
         return "login";
     }
 
+    @RequestMapping (value="/login", method=RequestMethod.POST)
+    public String doLogin (@ModelAttribute ("user") UserDTO user, Model model) {
+
+        //Hard-coded validation of user name and 
+        //password to keep this example simple
+        //But validation could be done against database or
+        //any other means here.
+        if ("admin".equals(user.getUserName()) && "admin".equals(user.getPassword())) {
+            return "redirect:courses";
+        }
+
+        user.setMessage("Invalid user name or password. Please try again");
+        return "login";
+    }
 }
