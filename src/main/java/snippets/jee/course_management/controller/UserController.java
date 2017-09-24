25c612c670452432d5b6b8abc57544785ae5b7f0
790013438 <790013438@qq.com>
2017-09-24 20:52:17 +0800

    更改session的方式
    用httpServletRequest.getSession().setAttribute("user", user);

--- src/main/java/snippets/jee/course_management/controller/UserController.java
+++ src/main/java/snippets/jee/course_management/controller/UserController.java
@@ -1,16 +1,16 @@
 package snippets.jee.course_management.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import snippets.jee.course_management.dto.UserDTO;
 
 @Controller
-@SessionAttributes("user")
 public class UserController {
 
     @RequestMapping (value="/login", method=RequestMethod.GET)
@@ -22,13 +22,14 @@ public class UserController {
     }
 
     @RequestMapping (value="/login", method=RequestMethod.POST)
-    public String doLogin (@ModelAttribute ("user") UserDTO user, Model model) {
+    public String doLogin (@ModelAttribute ("user") UserDTO user, Model model, HttpServletRequest httpServletRequest) {
 
         //Hard-coded validation of user name and 
         //password to keep this example simple
         //But validation could be done against database or
         //any other means here.
         if ("admin".equals(user.getUserName()) && "admin".equals(user.getPassword())) {
+            httpServletRequest.getSession().setAttribute("user", user);
             return "redirect:courses";
         }
 
